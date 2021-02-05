a20d30ad51bb8940bb303d0b44d74d41a0e3fdf1
Nika <nikasxirtla@hotmail.com>
2021-02-05 16:14:38 +0400

    Spinner change

--- app/src/main/java/com/example/a19/fragments/ConverterFragment.kt
+++ app/src/main/java/com/example/a19/fragments/ConverterFragment.kt
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.os.Bundle
 import android.preference.PreferenceManager
 import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Spinner
 import android.widget.TextView
@@ -12,16 +13,22 @@ import com.example.a19.R
 
 class ConverterFragment : Fragment(R.layout.fragment_converter) {
 
+    private var btcPrice : Float = 0.0f
+    private var ethPrice : Float = 0.0f
+    private var xrpPrice : Float = 0.0f
+    private var dotPrice : Float = 0.0f
+    private var adaPrice : Float = 0.0f
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val mText : TextView = view.findViewById(R.id.text_first)
+        val price_1 : TextView = view.findViewById(R.id.price_1)
+        val price_2 : TextView = view.findViewById(R.id.price_2)
         val prefs = PreferenceManager.getDefaultSharedPreferences(activity)
-        val btcPrice = prefs.getString("btc", "")
-        val ethPrice = prefs.getString("eth", "")
-        val xrpPrice = prefs.getString("xrp", "")
-        val dotPrice = prefs.getString("dot", "")
-        val adaPrice = prefs.getString("ada", "")
+        btcPrice = prefs.getString("btc", "0")!!.toFloat()
+        ethPrice = prefs.getString("eth", "0")!!.toFloat()
+        xrpPrice = prefs.getString("xrp", "0")!!.toFloat()
+        dotPrice = prefs.getString("dot", "0")!!.toFloat()
+        adaPrice = prefs.getString("ada", "0")!!.toFloat()
 
 
         val spinner2: Spinner = view.findViewById(R.id.spinner2)
@@ -31,6 +38,18 @@ class ConverterFragment : Fragment(R.layout.fragment_converter) {
                     adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
                     spinner2.adapter = adapter
                 }
+        }
+
+        // ესე იცვლება ვალუე-ბი
+        spinner2?.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                price_1.text = spinner2.getSelectedItem().toString()
+            }
+
         }
 
 
@@ -44,29 +63,18 @@ class ConverterFragment : Fragment(R.layout.fragment_converter) {
         }
 
 
-        mText.text = "BTC Price: $btcPrice \n ETH Price: $ethPrice \n XRP Price: $xrpPrice \n DOT Price: $dotPrice \n ADA Price: $adaPrice"
+        spinner3?.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                price_2.text = spinner3.getSelectedItem().toString()
+            }
+
+        }
+
         // marto btc, eth, xrp, dot, ada-s fasebia USD-shi
-
-        val text_first : TextView = view.findViewById(R.id.text_first)
-        if(view is TextView) {
-
-            var price: String = text_first.text.toString()
-            text_first.text = "1"
-
-
-
-        }
-
-        val text_price : TextView = view.findViewById(R.id.text_price)
-        if(view is TextView) {
-
-            val result: String = text_price.text.toString()
-            text_price.text = ""
-
-        }
-
-
-
 
     }
 
